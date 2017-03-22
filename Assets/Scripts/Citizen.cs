b999d5f7f99d2453df1bacdb5bdeeac62c8ec9b4
DESKTOP-791H0HN\Ian Chy <chrisian023@gmail.com>
2017-03-22 16:51:18 +0800

    +inherit pretender

--- Assets/Scripts/Citizen.cs
+++ Assets/Scripts/Citizen.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 [System.Serializable]
 public class Citizen {
@@ -14,16 +15,17 @@ public class Citizen {
 	public City city;
 	public ROLE role;
 	public Role assignedRole;
+	public HexTile assignedTile;
 	public List<BEHAVIOR_TRAIT> behaviorTraits;
 	public List<SKILL_TRAIT> skillTraits;
 	public List<MISC_TRAIT> miscTraits;
-	public Citizen supportedCitizen;
-	public Citizen possiblePretender;
+	public List<Citizen> supportedCitizen;
 	public Citizen father;
 	public Citizen mother;
 	public Citizen spouse;
 	public List<Citizen> children;
 	public CitizenChances citizenChances;
+	public CampaignManager campaignManager;
 	public MONTH birthMonth;
 	public int birthWeek;
 	public int birthYear;
@@ -35,9 +37,12 @@ public class Citizen {
 	public bool isGovernor;
 	public bool isKing;
 	public bool isPretender;
+	public bool isBusy;
 	public bool isDead;
 
+	private List<Citizen> possiblePretenders = new List<Citizen>();
 	private string history;
+
 
 	public Citizen(City city, int age, GENDER gender, int generation){
 		this.id = Utilities.SetID (this);
@@ -48,15 +53,19 @@ public class Citizen {
 		this.city = city;
 		this.role = ROLE.UNTRAINED;
 		this.assignedRole = null;
+		this.assignedTile = null;
+		this.behaviorTraits = new List<BEHAVIOR_TRAIT> ();
+		this.skillTraits = new List<SKILL_TRAIT> ();
+		this.miscTraits = new List<MISC_TRAIT> ();
 //		this.trait = GetTrait();
 //		this.trait = new TRAIT[]{TRAIT.VICIOUS, TRAIT.NONE};
-		this.supportedCitizen = null; //initially to king
-		this.possiblePretender = null;
+		this.supportedCitizen = new List<Citizen>(){this.city.kingdom.king}; //initially to king
 		this.father = null;
 		this.mother = null;
 		this.spouse = null;
 		this.children = new List<Citizen> ();
 		this.citizenChances = new CitizenChances ();
+		this.campaignManager = new CampaignManager (this);
 //		this.birthMonth = (MONTH) PoliticsPrototypeManager.Instance.month;
 //		this.birthWeek = PoliticsPrototypeManager.Instance.week;
 //		this.birthYear = PoliticsPrototypeManager.Instance.year;
@@ -66,6 +75,7 @@ public class Citizen {
 		this.isGovernor = false;
 		this.isKing = false;
 		this.isPretender = false;
+		this.isBusy = false;
 		this.isDead = false;
 		this.history = string.Empty;
 		this.city.citizens.Add (this);
@@ -77,6 +87,12 @@ public class Citizen {
 
 		EventManager.StartListening ("CitizenTurnActions", TurnActions);
 
+	}
+	internal int GetCampaignLimit(){
+		if(this.miscTraits.Contains(MISC_TRAIT.TACTICAL)){
+			return 3;
+		}
+		return 2;
 	}
 	internal void AddParents(Citizen father, Citizen mother){
 		this.father = father;
@@ -143,6 +159,12 @@ public class Citizen {
 	}
 	internal void Death(){
 //		this.kingdom.royaltyList.allRoyalties.Remove (this);
+		if(this.isPretender){
+			Citizen possiblePretender = GetPossiblePretender (this);
+			if(possiblePretender != null){
+				possiblePretender.isPretender = true;
+			}
+		}
 		this.city.kingdom.successionLine.Remove (this);
 		this.isDead = true;
 		EventManager.StopListening ("CitizenTurnActions", TurnActions);
@@ -160,12 +182,98 @@ public class Citizen {
 //			}
 //		}
 	}
+	private Citizen GetPossiblePretender(Citizen citizen){
+		this.possiblePretenders.Clear ();
+		ChangePossiblePretendersRecursively (citizen);
+		this.possiblePretenders.RemoveAt (0);
+		this.possiblePretenders.AddRange (GetSiblings (citizen));
 
+		List<Citizen> orderedMaleRoyalties = this.possiblePretenders.Where (x => x.gender == GENDER.MALE && x.generation > citizen.generation).OrderBy(x => x.generation).ThenByDescending(x => x.age).ToList();
+		if(orderedMaleRoyalties.Count > 0){
+			return orderedMaleRoyalties [0];
+		}else{
+			List<Citizen> orderedFemaleRoyalties = this.possiblePretenders.Where (x => x.gender == GENDER.FEMALE && x.generation > citizen.generation).OrderBy(x => x.generation).ThenByDescending(x => x.age).ToList();
+			if(orderedFemaleRoyalties.Count > 0){
+				return orderedFemaleRoyalties [0];
+			}else{
+				List<Citizen> orderedBrotherRoyalties = this.possiblePretenders.Where (x => x.gender == GENDER.MALE && x.father.id == citizen.father.id && x.id != citizen.id).OrderByDescending(x => x.age).ToList();
+				if(orderedBrotherRoyalties.Count > 0){
+					return orderedBrotherRoyalties [0];
+				}else{
+					List<Citizen> orderedSisterRoyalties = this.possiblePretenders.Where (x => x.gender == GENDER.FEMALE && x.father.id == citizen.id && x.id != citizen.id).OrderByDescending(x => x.age).ToList();
+					if(orderedSisterRoyalties.Count > 0){
+						return orderedSisterRoyalties [0];
+					}
+				}
+			}
+		}
+		return null;
+	}
+	internal List<Citizen> GetSiblings(Citizen citizen){
+		List<Citizen> siblings = new List<Citizen> ();
+		for(int i = 0; i < citizen.mother.children.Count; i++){
+			if(citizen.mother.children[i].id != citizen.id){
+				if(!citizen.mother.children[i].isDead){
+					siblings.Add (citizen.mother.children [i]);
+				}
+			}
+		}
+
+		return siblings;
+	}
+	private void ChangePossiblePretendersRecursively(Citizen citizen){
+		if(!citizen.isDead){
+			this.possiblePretenders.Add (citizen);
+		}
+
+		for(int i = 0; i < citizen.children.Count; i++){
+			if(citizen.children[i] != null){
+				this.ChangePossiblePretendersRecursively (citizen.children [i]);
+			}
+		}
+	}
 	internal void Campaign(CAMPAIGN type){
 		if(this.assignedRole != null){
 			if(this.assignedRole is General){
 				
 			}
 		}
+	}
+
+	internal void GiveTask(Citizen citizen){
+		if(citizen.assignedRole is General){
+			TaskForGenerals (citizen);
+		}
+	}
+
+	internal void TaskForGenerals(Citizen general){
+		Campaign chosenCampaign = null;
+		for(int i = 0; i < this.campaignManager.activeCampaigns.Count; i++){
+			if(this.campaignManager.activeCampaigns[i] != null){
+				if(!this.campaignManager.activeCampaigns[i].isFull && !this.campaignManager.activeCampaigns[i].hasStarted){
+					chosenCampaign = this.campaignManager.activeCampaigns [i];
+					break;
+				}
+			}
+		}
+
+		if(chosenCampaign != null){
+			chosenCampaign.registeredGenerals.Add (general);
+			AssignCampaignToGeneral (general, chosenCampaign);
+			if(chosenCampaign.registeredGenerals.Count >= this.GetArmyStrengthByPercentage(20f)){
+				chosenCampaign.isFull = true;
+			}
+		}
+	}
+	internal void AssignCampaignToGeneral(Citizen general, Campaign chosenCampaign){
+		((General)general.assignedRole).assignedCampaign = chosenCampaign.campaignType;
+		((General)general.assignedRole).targetCity = chosenCampaign.targetCity;
+		((General)general.assignedRole).targetLocation = chosenCampaign.targetCity.hexTile;
+		((General)general.assignedRole).location = general.assignedTile;
+	}
+	internal int GetArmyStrengthByPercentage(float percent){
+		float percentage = 0f; // (percent / 100f) * this.allControlledGenerals.Count;
+
+		return (int)percentage;
 	}
 }
