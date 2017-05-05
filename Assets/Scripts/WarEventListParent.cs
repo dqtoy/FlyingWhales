f8eb34714f5ce67ad927d7ad2edfe75f28e58357
Mykel Abania <mntabania@gmail.com>
2017-05-05 19:09:47 +0800

    *UI Fixes

---
+++ Assets/Scripts/WarEventListParent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarEventListParent : MonoBehaviour {
+
+	public GameObject anchorPoint;
+	public UIAnchor anchor;
+	public UILabel eventTitleLbl;
+	public UIGrid eventsGrid;
+
+	public GameObject arrow;
+	public TweenRotation tweenRotation;
+
+	public int targetKingdomID;
+
+	public void ToggleList(){
+		eventsGrid.gameObject.SetActive(!eventsGrid.gameObject.activeSelf);
+	}
+
+	public void Reset(){
+		arrow.transform.localEulerAngles = new Vector3 (0f, 0f, -90f);
+		tweenRotation.from = new Vector3 (0f, 0f, -90f);
+		tweenRotation.to = new Vector3 (0f, 0f, -180f);
+		eventsGrid.gameObject.SetActive(false);
+	}
+}
