5a58d740f7327500e0aea1921193476297ac619c
Mykel Abania <mntabania@gmail.com>
2017-05-17 12:32:07 +0800

    *Localization Manager Fixes

--- Assets/Scripts/Localization/LocalizationManager.cs
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -23,41 +23,49 @@ public class LocalizationManager : MonoBehaviour {
 
 		this.language = Utilities.defaultLanguage;
 		this.filePath = Application.streamingAssetsPath + "/" + this.language.ToString();
-
-
+		LoadLocalizedTexts ();
 	}
 
-	public void LoadLocalizedTexts()
-	{
+	/*
+	 * Load Localized Text From the StreamingAssets
+	 * Directory, and put them in the localizedText Dictionary
+	 * by category then by file name.
+	 * e.g. localizedText["Events"]["BorderConflict"]
+	 * */
+	public void LoadLocalizedTexts(){
 		this.localizedText.Clear ();
-//		string filePath = Path.Combine (this.filePath, fileName);
 		string[] directories = Directory.GetDirectories(this.filePath);
 		for (int i = 0; i < directories.Length; i++) {
-			string subFilePath = this.filePath + "/" + directories [i];
-			string[] files = Directory.GetFiles (subFilePath);
-			this.localizedText.Add (directories [i], new Dictionary<string, Dictionary<string, string>>());
+			string categoryName = Path.GetFileName(directories [i]);
+			if (!this.localizedText.ContainsKey(categoryName)) {
+				this.localizedText.Add (categoryName, new Dictionary<string, Dictionary<string, string>> ());
+			}
+			string[] files = Directory.GetFiles(directories[i], "*.json");
 			for (int j = 0; j < files.Length; j++) {
-				string actualFilePath = subFilePath + "/" + files [j];
-				this.localizedText [directories [i]].Add (files [j], new Dictionary<string, string> ());   
-				if (File.Exists (actualFilePath)) {
-					string dataAsJson = File.ReadAllText (actualFilePath);
+				string fileName = Path.GetFileNameWithoutExtension(files[j]);
+				if (!this.localizedText.ContainsKey(fileName)) {
+					this.localizedText [categoryName].Add (fileName, new Dictionary<string, string> ());
+				}
+				if (File.Exists(files[j])) {
+					string dataAsJson = File.ReadAllText(files[j]);
 					LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
 
 					for (int k = 0; k < loadedData.items.Length; k++) {
-						this.localizedText[directories[i]][files[j]].Add (loadedData.items [k].key, loadedData.items [k].value);   
+						this.localizedText[categoryName][fileName].Add(loadedData.items [k].key, loadedData.items [k].value);   
 					}
-
 					Debug.Log ("Data loaded, dictionary contains: " + this.localizedText.Count + " entries");
 				} else {
 					Debug.LogError ("Cannot find file!");
 				}
 			}
 		}
-
-
 		this.isReady = true;
 	}
 
+	/*
+	 * Get value of a key in a category in the
+	 * localizedText Dictionary.
+	 * */
 	public string GetLocalizedValue(string category, string file, string key){
 		string result = this.missingTextString;
 		if (this.localizedText[category][file].ContainsKey (key)){
@@ -68,6 +76,11 @@ public class LocalizationManager : MonoBehaviour {
 
 	}
 
+	/*
+	 * Get if the LocalizationManager has loaded
+	 * all the necessary files into the localizedText
+	 * Dictionary.
+	 * */
 	public bool GetIsReady(){
 		return isReady;
 	}
