8f564d5993b8626d4981a251bd99e1ad60bc4859
Mykel Abania <mntabania@gmail.com>
2017-05-23 17:49:06 +0800

    +Camera clamp movement to map bounds

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -9,57 +9,109 @@ public class CameraMove : MonoBehaviour {
 //	float maxFov = 150f;
 //	float sensitivity = 20f;
 
-	float minFov = 7f;
-	float maxFov = 54f;
-	float sensitivity = 5f;
+	[SerializeField] private float minFov;
+	[SerializeField] private float maxFov;
+	[SerializeField] private float sensitivity;
+	[SerializeField] private Camera eventIconCamera;
+	[SerializeField] private Camera resourceIconCamera;
+	[SerializeField] private Camera generalCamera;
+	[SerializeField] private Camera traderCamera;
 
-
-	public float dampTime = 0.2f;
+	private float dampTime = 0.2f;
 	private Vector3 velocity = Vector3.zero;
-	public Transform target;
+	private Transform target;
 
-	public Camera eventIconCamera;
-	public Camera resourceIconCamera;
-	public Camera generalCamera;
-    public Camera traderCamera;
+	//default camera bounds when fov is at minimum
+	const float minMIN_X = 12.5f;
+	const float minMAX_X = 189.5f;
+	const float minMIN_Y = 6.5f;
+	const float minMAX_Y = 143.5f;
 
-    private float MIN_X = 66f;
-	private float MAX_X = 126f;
-	private float MIN_Y = 36f;
-	private float MAX_Y = 92f;
+	//default camera bounds when fov is at maximum
+	const float maxMIN_X = 44f;
+	const float maxMAX_X = 158f;
+	const float maxMIN_Y = 24.5f;
+	const float maxMAX_Y = 126f;
+
+	const float MIN_Z = -10f;
+	const float MAX_Z = -10f;
+
+	private float MIN_X;
+	private float MAX_X;
+	private float MIN_Y;
+	private float MAX_Y;
 
 	void Awake(){
 		Instance = this;
+		MIN_X = minMIN_X;
+		MAX_X = minMAX_X;
+		MIN_Y = minMIN_Y;
+		MAX_Y = minMAX_Y;
 	}
 
 	void Update () {
-		float xAxisValue = Input.GetAxis("Horizontal");
-		float zAxisValue = Input.GetAxis("Vertical");
-		if(Camera.current != null){
-			Camera.main.transform.Translate(new Vector3(xAxisValue, zAxisValue, 0.0f));
-		}
-
-		if (!UIManager.Instance.IsMouseOnUI ()) {
+		Rect screenRect = new Rect(0,0, Screen.width, Screen.height);
+		if (!UIManager.Instance.IsMouseOnUI() && screenRect.Contains(Input.mousePosition)) { //enable scrolling only when mouse is not on UI and is inside game window
+			//camera scrolling code
 			float fov = Camera.main.orthographicSize;
-			fov += Input.GetAxis ("Mouse ScrollWheel") * (sensitivity * -1f);
+			float adjustment = Input.GetAxis ("Mouse ScrollWheel") * (sensitivity * -1f);
+			fov += adjustment;
 			fov = Mathf.Clamp (fov, minFov, maxFov);
 			Camera.main.orthographicSize = fov;
 			eventIconCamera.orthographicSize = fov;
 			resourceIconCamera.orthographicSize = fov;
 			generalCamera.orthographicSize = fov;
+
+			//adjust camera movement clamps
+			if (adjustment > 0f) {
+				MAX_X -= .9f;
+				MAX_X = Mathf.Clamp (MAX_X, maxMAX_X, minMAX_X);
+
+				MIN_X += .9f;
+				MIN_X = Mathf.Clamp (MIN_X, minMIN_X, maxMIN_X);
+
+				MAX_Y -= .5f;
+				MAX_Y = Mathf.Clamp (MAX_Y, maxMAX_Y, minMAX_Y);
+
+				MIN_Y += .5f;
+				MIN_Y = Mathf.Clamp (MIN_Y, minMIN_Y, maxMIN_Y);
+			} else if (adjustment < 0f) {
+				MAX_X += .9f;
+				MAX_X = Mathf.Clamp (MAX_X, maxMAX_X, minMAX_X);
+
+				MIN_X -= .9f;
+				MIN_X = Mathf.Clamp (MIN_X, minMIN_X, maxMIN_X);
+
+				MAX_Y += .5f;
+				MAX_Y = Mathf.Clamp (MAX_Y, maxMAX_Y, minMAX_Y);
+
+				MIN_Y -= .5f;
+				MIN_Y = Mathf.Clamp (MIN_Y, minMIN_Y, maxMIN_Y);
+			}
+			Camera.main.transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x, MIN_X, MAX_X),
+				Mathf.Clamp(transform.position.y, MIN_Y, MAX_Y),
+				Mathf.Clamp(transform.position.z, MIN_Z, MAX_Z));
 		}
 
-//		transform.position = new Vector3(
-//			Mathf.Clamp(transform.position.x, MIN_X, MAX_X),
-//			Mathf.Clamp(transform.position.y, MIN_Y, MAX_Y),
-//			Mathf.Clamp(transform.position.z, -10, -10));
-
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float zAxisValue = Input.GetAxis("Vertical");
+		if(Camera.current != null){
+			//camera movement code
+			Camera.main.transform.Translate(new Vector3(xAxisValue, zAxisValue, 0.0f));
+			Camera.main.transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x, MIN_X, MAX_X),
+				Mathf.Clamp(transform.position.y, MIN_Y, MAX_Y),
+				Mathf.Clamp(transform.position.z, MIN_Z, MAX_Z));
+		}
+			
 		if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.RightArrow) ||
 		   Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D)) {
+			//reset target when player pushes a button to pan the camera
 			target = null;
 		}
-
-		if (target) {
+			
+		if (target) { //smooth camera center
 			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
 			Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			Vector3 destination = transform.position + delta;
