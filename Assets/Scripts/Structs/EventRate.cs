0171fae9b0caaaea46a030bccaf302a7f0549b73
Marvin\Marvin Apacible <marvin.apacible@yahoo.com>
2017-05-23 12:33:37 +0800

    - updated Kingdom Type Data structure

---
+++ Assets/Scripts/Structs/EventRate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct EventRate {
+	public EVENT_TYPES eventType;
+	public int rate;
+	public RELATIONSHIP_STATUS[] relationshipTargets;
+	public KINGDOM_TYPE[] kingdomTypes;
+	public MILITARY_STRENGTH[] militaryStrength;
+
+	public EventRate(EVENT_TYPES eventType, int rate, RELATIONSHIP_STATUS[] relationshipTargets, KINGDOM_TYPE[] kingdomTypes, MILITARY_STRENGTH[] militaryStrength) {
+		this.eventType = eventType;
+		this.rate = rate;
+		this.relationshipTargets = relationshipTargets;
+		this.kingdomTypes = kingdomTypes;
+		this.militaryStrength = militaryStrength;
+	}
+}
