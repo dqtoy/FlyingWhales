40700e2f963d003153a5eb760209a8947c6df326
Ian Chy <chrisian023@gmail.com>
2017-06-23 19:09:49 +0800

    *recommit boon of power testing ui, *bug fix - on end attack on general

---
+++ Assets/Scripts/AvatarAnimationEvents.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarAnimationEvents : MonoBehaviour {
+
+	public GameObject avatarGO;
+
+	public void OnEndAttack(){
+		Role role = this.avatarGO.GetComponent<Avatar> ().citizen.assignedRole;
+		if(role is General){
+			this.avatarGO.GetComponent<GeneralAvatar> ().OnEndAttack ();
+		}
+	}
+}
