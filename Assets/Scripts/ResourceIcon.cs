f0092fe1edbdd85404a1631bd800e195c22f1c4e
Ian Chy <chrisian023@gmail.com>
2017-08-16 17:38:55 +0800

    *resource icons location moved, *tile related prefabs changed to static

--- Assets/Scripts/ResourceIcon.cs
+++ Assets/Scripts/ResourceIcon.cs
@@ -7,6 +7,8 @@ public class ResourceIcon : MonoBehaviour {
     private RESOURCE _resource;
     private bool isHovering = false;
 
+	public Sprite[] icons;
+
     #region getters/setters
     public RESOURCE resource {
         get { return _resource; }
@@ -15,13 +17,22 @@ public class ResourceIcon : MonoBehaviour {
 
     public void SetResource(RESOURCE resource) {
         _resource = resource;
-        if(gameObject.GetComponent<SpriteRenderer>() != null) {
-            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.LoadAll<Sprite>("Resources Icons")
-                    .Where(x => x.name == resource.ToString()).ToList()[0];
-        } else if(gameObject.GetComponent<UI2DSprite>() != null) {
-            gameObject.GetComponent<UI2DSprite>().sprite2D = Resources.LoadAll<Sprite>("Resources Icons")
-                    .Where(x => x.name == resource.ToString()).ToList()[0];
-        }
+		for (int i = 0; i < this.icons.Length; i++) {
+			if(this.icons[i].name == resource.ToString()){
+				if(gameObject.GetComponent<SpriteRenderer>() != null) {
+					gameObject.GetComponent<SpriteRenderer> ().sprite = this.icons [i];
+				} else if(gameObject.GetComponent<UI2DSprite>() != null) {
+					gameObject.GetComponent<UI2DSprite>().sprite2D = this.icons [i];
+				}
+			}
+		}
+//        if(gameObject.GetComponent<SpriteRenderer>() != null) {
+//            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.LoadAll<Sprite>("Resources Icons")
+//                    .Where(x => x.name == resource.ToString()).ToList()[0];
+//        } else if(gameObject.GetComponent<UI2DSprite>() != null) {
+//            gameObject.GetComponent<UI2DSprite>().sprite2D = Resources.LoadAll<Sprite>("Resources Icons")
+//                    .Where(x => x.name == resource.ToString()).ToList()[0];
+//        }
         gameObject.SetActive(true);
     }
 
