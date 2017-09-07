f3e88bf1fc1290998aacd052a33d049c3170f2b2
Mykel Abania <mntabania@gmail.com>
2017-09-07 20:00:50 +0800

    *Get Tiles in Range optimization

---
+++ Assets/Scripts/Structs/CubeCoordinate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct CubeCoordinate {
+    public int x;
+    public int y;
+    public int z;
+
+    public CubeCoordinate(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ Assets/Scripts/Structs/HexCoordinate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct HexCoordinate {
+    public int col;
+    public int row;
+
+    public HexCoordinate(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
+}
