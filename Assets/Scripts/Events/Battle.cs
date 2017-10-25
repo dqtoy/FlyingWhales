eb332f5455e62d7059a3dcf640524dbf116ed2bf
Ian Chy <chrisian023@gmail.com>
2017-10-25 22:43:13 +0800

    *There are corpse mounds in water tiles

--- Assets/Scripts/Events/Battle.cs
+++ Assets/Scripts/Events/Battle.cs
@@ -644,7 +644,7 @@ public class Battle {
 		}
 
 		for (int i = 0; i < defenderRegion.outerTiles.Count; i++) {
-			if(defenderRegion.outerTiles[i].corpseMound == null && defenderRegion.outerTiles[i].IsAdjacentWithRegion(attackerRegion)){
+			if(defenderRegion.outerTiles[i].elevationType != ELEVATION.WATER && defenderRegion.outerTiles[i].corpseMound == null && defenderRegion.outerTiles[i].IsAdjacentWithRegion(attackerRegion)){
 				defenderRegion.outerTiles [i].CreateCorpseMoundObjectOnTile (amount);
 				return;
 			}
