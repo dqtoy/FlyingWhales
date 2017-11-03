9fefad81f39ab3e656f6d74ff14f20cc15ee3fcc
Ian Chy <chrisian023@gmail.com>
2017-11-03 20:59:56 +0800

    *name plates

---
+++ Assets/Scripts/NoScaleGO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoScaleGO : MonoBehaviour {
+
+	private float orthoOrg;
+	private float orthoCurr;
+	private Vector3 scaleOrg;
+	private Vector3 posOrg;
+
+	void Start() {
+		orthoOrg = Camera.main.orthographicSize;
+		orthoCurr = orthoOrg;
+		scaleOrg = transform.localScale;
+		posOrg = Camera.main.WorldToViewportPoint(transform.position);
+	}
+	void Update() {
+		var osize = Camera.main.orthographicSize;
+		if (orthoCurr != osize) {
+			float minus = 0.04f * (orthoOrg - osize);
+			Vector3 size = scaleOrg * orthoOrg / osize;
+			transform.localScale = new Vector3(size.x - minus, size.y - minus, size.z - minus);
+			orthoCurr = osize;
+//			transform.position = Camera.main.ViewportToWorldPoint(posOrg);
+		}
+
+	}
+}
