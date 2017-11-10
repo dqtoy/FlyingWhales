569bee3e7275381cdaa1298093a33e80f58ef73e
Mykel Abania <mntabania@gmail.com>
2017-11-10 21:50:40 +0800

    *Initial History UI

---
+++ Assets/Scripts/KingdomEmblem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KingdomEmblem : MonoBehaviour {
+
+    private Kingdom _kingdom;
+
+    [SerializeField] private UI2DSprite _emblemBG;
+    [SerializeField] private UI2DSprite _emblemSprite;
+    [SerializeField] private UI2DSprite _emblemOutline;
+
+    internal void SetKingdom(Kingdom kingdom) {
+        _kingdom = kingdom;
+
+        _emblemBG.sprite2D = kingdom.emblemBG;
+        _emblemSprite.sprite2D = kingdom.emblem;
+        _emblemOutline.sprite2D = kingdom.emblemBG;
+    }
+}
