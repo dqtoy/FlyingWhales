847602adfbdbbd4d89c6ccf589f98efb6e8fa68d
Ian Chy <chrisian023@gmail.com>
2017-12-08 18:11:08 +0800

    *body part creator

---
+++ Assets/CombatPrototype/Scripts/BodyParts/IBodyPart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ECS{
+	public class IBodyPart: EntityComponent {
+		public enum IMPORTANCE {
+			ESSENTIAL,
+			NON_ESSENTIAL,
+		}
+
+		public enum ATTRIBUTE {
+			CLAWED,
+			CAN_PUNCH,
+			CAN_GRIP,
+		}
+
+		public enum STATUS {
+			NORMAL,
+			INJURED,
+			DECAPITATED,
+		}
+	}
+}
+
---
+++ Assets/CombatPrototype/Scripts/Character.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ECS{
+	public class Character: EntityComponent {
+		
+	}
+}
+
