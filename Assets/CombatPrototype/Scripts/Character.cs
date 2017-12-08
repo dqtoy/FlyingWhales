a1f0ef0d32f9e63cd8f40639b036fbd2b8cf77bb
Ian Chy <chrisian023@gmail.com>
2017-12-08 19:24:08 +0800

    +Character

--- Assets/CombatPrototype/Scripts/Character.cs
+++ Assets/CombatPrototype/Scripts/Character.cs
@@ -1,9 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace ECS{
-	public class Character: EntityComponent {
-		
+	public class Character: MonoBehaviour {
+		[SerializeField] private int _maxHP;
+		public TextAsset characterClassJson;
+		public TextAsset bodyPartsJson;
+
+		private string _name;
+		private List<Trait>	_traits;
+		private int	_level;
+		private int	_currentHP;
+		private List<BodyPart> _bodyParts;
+		private CharacterClass _characterClass;
+
+
+		void Start(){
+			BodyPartsData bodyPartsData = JsonUtility.FromJson<BodyPartsData> (bodyPartsJson.text);
+			this._bodyParts = new List<BodyPart> (bodyPartsData.bodyParts);
+			this._characterClass = new CharacterClass ();
+			this._characterClass = JsonUtility.FromJson<CharacterClass> (characterClassJson.text);
+		}
 	}
 }
 
