477ad6d03c55a42d9368dae12561dedd235cc72c
Ian Chy <chrisian023@gmail.com>
2017-12-11 19:51:14 +0800

    *initial combat sim

---
+++ Assets/CombatPrototype/Scripts/CombatPrototype.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ECS{
+	public enum SIDES{
+		A,
+		B,
+	}
+
+	public class CombatPrototype : MonoBehaviour {
+		public CombatPrototype Instance;
+
+		public Dictionary<SIDES, List<Character>> allCharactersAndSides;
+
+		void Awake(){
+			Instance = this;
+		}
+
+		void Start(){
+			this.allCharactersAndSides = new Dictionary<SIDES, List<Character>> ();
+		}
+
+		//Add a character to a side
+		internal void AddCharacter(SIDES side, Character character){
+			if(this.allCharactersAndSides.ContainsKey(side)){
+				this.allCharactersAndSides [side].Add (character);
+			}else{
+				this.allCharactersAndSides.Add (side, new List<Character>(){character});
+			}
+		}
+
+		//Remove a character from a side
+		internal void RemoveCharacter(SIDES side, Character character){
+			if(this.allCharactersAndSides.ContainsKey(side)){
+				this.allCharactersAndSides [side].Remove (character);
+			}
+		}
+
+		//This simulates the whole combat system
+		internal void CombatSimulation(){
+
+		}
+	}
+}
+
---
+++ Assets/CombatPrototype/Scripts/CombatPrototypeUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ECS{
+	public class CombatPrototypeUI : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
