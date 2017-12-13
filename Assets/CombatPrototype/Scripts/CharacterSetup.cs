a16b017bc6fa12fbf71b6fc2bb0f16c106fc7b5e
Mykel Abania <mntabania@gmail.com>
2017-12-13 10:06:28 +0800

    *Character Setup Creator

---
+++ Assets/CombatPrototype/Scripts/CharacterSetup.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ECS {
+    [System.Serializable]
+    public class CharacterSetup {
+        public CharacterClass characterClass;
+        public RaceSetting raceSetting;
+    }
+}
