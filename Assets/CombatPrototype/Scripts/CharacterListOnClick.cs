e1de1159e2ae2c6bd65f77e6496d58fed7a29864
Ian Chy <chrisian023@gmail.com>
2017-12-13 11:15:00 +0800

    *character summary

---
+++ Assets/CombatPrototype/Scripts/CharacterListOnClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ECS{
+	public class CharacterListOnClick : MonoBehaviour {
+
+		void OnClick(){
+			UILabel lbl = GetComponent<UILabel> ();
+			string url = lbl.GetUrlAtPosition (UICamera.lastWorldPosition);
+			if (!string.IsNullOrEmpty (url)) {
+				string id = url.Substring (0, url.IndexOf ('_'));
+				int idToUse = int.Parse (id);
+				if(url.Contains("_sideA")){
+//					CombatPrototype.Instance.charactersSideA [idToUse];
+				}else if(url.Contains("_sideB")){
+//					CombatPrototype.Instance.charactersSideB [idToUse];
+				}
+			}
+		}
+	}
+}
+
---
+++ Assets/CombatPrototype/Scripts/CharacterSummary.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ECS{
+	public class CharacterSummary : MonoBehaviour {
+		public CharacterSummary Instance;
+
+		public UILabel basicInfoLbl;
+		public UILabel classInfoLbl;
+		public UILabel bodyPartsInfoLbl;
+
+		void Awake(){
+			Instance = this;
+		}
+
+		public void ShowCharacterSummary(Character character){
+			
+
+		}
+
+//		private void BasicInfo(Character character){
+//			summaryLbl 
+//		}
+
+	}
+}
+
