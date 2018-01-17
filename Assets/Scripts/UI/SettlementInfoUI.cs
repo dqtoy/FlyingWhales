a53c08faae7ea660b9ab7a163a63b5491e6ed36f
Ian Chy <chrisian023@gmail.com>
2018-01-17 18:32:18 +0800

    *landmark info ui

--- Assets/Scripts/UI/SettlementInfoUI.cs
+++ Assets/Scripts/UI/SettlementInfoUI.cs
@@ -43,10 +43,10 @@ public class SettlementInfoUI : UIMenu {
         }
         string text = string.Empty;
 		text += "[b]Location:[/b] " + "[url=" + currentlyShowingSettlement.location.id + "_hextile]" + currentlyShowingSettlement.location.tileName + "[/url]";
-		text += "[b]Can Be Occupied:[/b] " + currentlyShowingSettlement.canBeOccupied.ToString();
-		text += "[b]Is Occupied:[/b] " + currentlyShowingSettlement.isOccupied.ToString();
-		text += "[b]Is Hidden:[/b] " + currentlyShowingSettlement.isHidden.ToString();
-		text += "[b]Is Explored:[/b] " + currentlyShowingSettlement.isExplored.ToString();
+		text += "\n[b]Can Be Occupied:[/b] " + currentlyShowingSettlement.canBeOccupied.ToString();
+		text += "\n[b]Is Occupied:[/b] " + currentlyShowingSettlement.isOccupied.ToString();
+		text += "\n[b]Is Hidden:[/b] " + currentlyShowingSettlement.isHidden.ToString();
+		text += "\n[b]Is Explored:[/b] " + currentlyShowingSettlement.isExplored.ToString();
 
         if (currentlyShowingSettlement.owner != null) {
 			text += "\n[b]Owner:[/b] " + "[url=" + currentlyShowingSettlement.owner.id + "_faction]" + currentlyShowingSettlement.owner.name + "[/url]" + "/" + currentlyShowingSettlement.owner.race.ToString();
