a65e0582662d28118a9539583b3f71601229c5da
Ian Chy <chrisian023@gmail.com>
2018-01-22 20:07:28 +0800

    +encounter party initial setup

---
+++ Assets/CombatPrototype/Scripts/EncounterParty.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EncounterParty : MonoBehaviour {
+	public TextAsset[] partyMembers;
+
+	private List<ECS.CharacterSetup> _characterSetups;
+
+	void Start(){
+		_characterSetups = new List<ECS.CharacterSetup> ();
+	}
+
+	private void ConstructAllCharacters(){
+		string path = "Assets/CombatPrototype/Data/CharacterSetups/";
+		for (int i = 0; i < partyMembers.Length; i++) {
+			string file = path + partyMembers[i].name + ".json";
+			string dataAsJson = System.IO.File.ReadAllText(file);
+			ECS.CharacterSetup charSetup = JsonUtility.FromJson<ECS.CharacterSetup>(dataAsJson);
+			_characterSetups.Add(charSetup);
+		}	
+	}
+
+	internal List<ECS.CharacterSetup> GetAllCharacters(){
+		return new List<ECS.CharacterSetup> (_characterSetups);
+	}
+}
