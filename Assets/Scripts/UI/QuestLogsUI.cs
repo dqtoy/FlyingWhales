ae79f3c5136f8d471c39d92b12ca36abb7f5cc49
Mykel Abania <mntabania@gmail.com>
2018-01-23 19:54:30 +0800

    *Quest Logs UI *Explore Button Fix

---
+++ Assets/Scripts/UI/QuestLogsUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestLogsUI : UIMenu {
+
+    internal bool isShowing = false;
+
+    [SerializeField] private UILabel questLogsLbl;
+
+    private Quest currentlyShowingQuest;
+
+    public void ShowQuestLogs(Quest quest) {
+        currentlyShowingQuest = quest;
+        isShowing = true;
+        this.gameObject.SetActive(true);
+    }
+
+    public void HideQuestLogs() {
+        isShowing = false;
+        this.gameObject.SetActive(false);
+    }
+
+    public void UpdateQuestLogs() {
+        string text = string.Empty;
+        for (int i = 0; i < currentlyShowingQuest.questLogs.Count; i++) {
+            string currLog = currentlyShowingQuest.questLogs[i];
+            text += currLog + "\n";
+        }
+        questLogsLbl.text = text;
+    }
+}
