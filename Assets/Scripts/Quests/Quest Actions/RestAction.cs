3b2e1eaa674612f0a9fcbf639c57cfe4eda2d62c
Mykel Abania <mntabania@gmail.com>
2018-01-25 16:14:50 +0800

    *Rest regen amount configurable

--- Assets/Scripts/Quests/Quest Actions/RestAction.cs
+++ Assets/Scripts/Quests/Quest Actions/RestAction.cs
@@ -28,7 +28,7 @@ public class RestAction : QuestAction {
     public void Rest() {
         for (int i = 0; i < charactersToRest.Count; i++) {
             ECS.Character currCharacter = charactersToRest[i];
-            currCharacter.AdjustHP(GetDailyRestHealAmount(currCharacter.raceSetting.race));
+            currCharacter.AdjustHP(currCharacter.raceSetting.restRegenAmount);
         }
         CheckIfCharactersAreFullyRested(charactersToRest);
     }
@@ -44,22 +44,6 @@ public class RestAction : QuestAction {
         }
         if (allCharactersRested) {
             ActionDone(QUEST_ACTION_RESULT.SUCCESS);
-        }
-    }
-    private int GetDailyRestHealAmount(RACE race) {
-        switch (race) {
-            case RACE.HUMANS:
-                return 20; //Humans: 20HP per day
-            case RACE.ELVES:
-                return 30; //Elves: 30HP per day
-            case RACE.GOBLIN:
-                return 10; //Goblins: 10HP per day
-            case RACE.TROLL:
-                return 50; //Trolls: 50HP per day
-            case RACE.DRAGON:
-                return 5; //Dragons: 5HP per day
-            default:
-                return 0;
         }
     }
 }
