ce53ae8ee920f1d4fe2d183a34d2f89142a7c952
Ian Chy <chrisian023@gmail.com>
2018-02-09 16:46:18 +0800

    *repush construction

--- Assets/Scripts/Production/Construction.cs
+++ Assets/Scripts/Production/Construction.cs
@@ -6,16 +6,5 @@ public class Construction : MonoBehaviour {
 	public Structure structure;
 	public TECHNOLOGY technology;
 	public Production production;
-	public List<StructureMaterial> structureMaterials;
-
-	private Dictionary<MATERIAL, StructureMaterial> _structureMaterialsLookup;
-
-	void Awake(){
-		if(structureMaterials != null && structureMaterials.Count > 0){
-			_structureMaterialsLookup = new Dictionary<MATERIAL, StructureMaterial> ();
-			for (int i = 0; i < structureMaterials.Count; i++) {
-				_structureMaterialsLookup.Add (structureMaterials [i].material, structureMaterials [i]);
-			}
-		}
-	}
+	public List<MATERIAL> materials;
 }
