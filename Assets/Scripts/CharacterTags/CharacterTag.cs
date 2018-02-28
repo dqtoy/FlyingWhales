c3e94ce3d5fb8ad762c6567c70ccc59388e8494d
Ian Chy <chrisian023@gmail.com>
2018-03-01 00:21:40 +0800

    +character tag setup

---
+++ Assets/Scripts/CharacterTags/CharacterTag.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterTag {
+	protected ECS.Character _character;
+	protected string _tagName;
+	protected CHARACTER_TAG _tagType;
+	protected List<CharacterTask> _tagTasks;
+
+	#region getters/setters
+	public CHARACTER_TAG tagType {
+		get { return _tagType; }
+	}
+	public ECS.Character character{
+		get { return _character; }
+	}
+	public List<CharacterTask> tagTasks {
+		get { return _tagTasks; }
+	}
+	#endregion
+
+	public CharacterTag(ECS.Character character){
+		_character = character;
+	}
+}
