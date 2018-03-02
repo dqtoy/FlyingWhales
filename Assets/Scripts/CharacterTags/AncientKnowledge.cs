1e630dd259f45e66daf69e04fca905161c4eba7e
Ian Chy <chrisian023@gmail.com>
2018-03-02 11:12:54 +0800

    +ancient knowledge tag

---
+++ Assets/Scripts/CharacterTags/AncientKnowledge.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AncientKnowledge : CharacterTag {
+	public AncientKnowledge(ECS.Character character): base(character, CHARACTER_TAG.ANCIENT_KNOWLEDGE){
+	}
+}
