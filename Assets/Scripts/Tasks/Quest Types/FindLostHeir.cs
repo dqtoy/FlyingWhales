03c551120f116c32a4c978870ac9f7a033f36c55
Mykel Abania <mntabania@gmail.com>
2018-03-08 20:18:48 +0800

    +Initial Find Lost Heir

---
+++ Assets/Scripts/Tasks/Quest Types/FindLostHeir.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindLostHeir : Quest {
+    public FindLostHeir(TaskCreator createdBy) : base(createdBy, QUEST_TYPE.FIND_LOST_HEIR) {
+    }
+}
