001266eee48eb309d99bbbb03ac76db81da9e6d8
Mykel Abania <mntabania@gmail.com>
2018-03-23 17:35:53 +0800

    *Lost Heir explore fix

--- Assets/Scripts/CharacterTags/Hermit.cs
+++ Assets/Scripts/CharacterTags/Hermit.cs
@@ -9,7 +9,7 @@ public class Hermit : CharacterRole {
         _roleTasks.Add(new DoNothing(this._character));
         _roleTasks.Add(new Rest(this._character));
         _roleTasks.Add(new MoveTo(this._character));
-        _roleTasks.Add(new ExploreTile(this._character));
+        _roleTasks.Add(new ExploreTile(this._character, 5));
         _defaultRoleTask = _roleTasks[2];
     }
 }
