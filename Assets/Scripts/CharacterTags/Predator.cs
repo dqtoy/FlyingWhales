a159f1e2b548406d9026dfa061c76f7b2ab219ad
Mykel Abania <mntabania@gmail.com>
2018-03-27 19:04:58 +0800

    +Predator Tag

---
+++ Assets/Scripts/CharacterTags/Predator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using ECS;
+
+public class Predator : CharacterTag {
+    public Predator(Character character) : base(character, CHARACTER_TAG.PREDATOR) {
+        _tagTasks.Add(new HuntPrey(_character));
+    }
+}
