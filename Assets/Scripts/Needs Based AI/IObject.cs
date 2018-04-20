4a5d571abd8454387a43388ea02df19686b0870d
Ian Chy <chrisian023@gmail.com>
2018-04-20 19:21:17 +0800

    +iobject, +character action, +object state, +reward, +action filter (no data yet)

---
+++ Assets/Scripts/Needs Based AI/IObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IObject {
+    //Data
+    List<ObjectState> states { get; }
+    bool isInvisible { get; }
+    int foodAdvertisementValue { get; }
+    int energyAdvertisementValue { get; }
+    int joyAdvertisementValue { get; }
+    int prestigeAdvertisementValue { get; }
+}
