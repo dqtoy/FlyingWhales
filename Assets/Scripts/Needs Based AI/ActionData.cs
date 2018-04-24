222eccf7eb060b448ed4d54d08bba61117e5c642
Ian Chy <chrisian023@gmail.com>
2018-04-24 18:06:56 +0800

    *todo add filter

--- Assets/Scripts/Needs Based AI/ActionData.cs
+++ Assets/Scripts/Needs Based AI/ActionData.cs
@@ -75,6 +75,7 @@ public class ActionData {
             for (int j = 0; j < landmark.objects.Count; j++) {
                 IObject iobject = landmark.objects[j];
                 for (int k = 0; k < iobject.currentState.actions.Count; k++) {
+                    //TODO: Add Filter Here, put this code inside something like if meetsfilterrequirements
                     CharacterAction action = iobject.currentState.actions[k];
                     int advertisement = action.GetTotalAdvertisementValue(_character);
                     actionLog += "\n" + action.actionData.actionName + " = " + advertisement + " (" + iobject.objectName + ")";
