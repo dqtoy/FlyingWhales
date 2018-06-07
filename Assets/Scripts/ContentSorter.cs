7785b39b6ab217fa2298c420aaab54ca69dbc8cf
Ian Chy <chrisian023@gmail.com>
2018-06-07 20:06:28 +0800

    *main menu, *content sorter hide inactive

--- Assets/Scripts/ContentSorter.cs
+++ Assets/Scripts/ContentSorter.cs
@@ -13,10 +13,18 @@ public enum SortingOrder {
     Descending,
 }
 public class ContentSorter : MonoBehaviour {
+    /// <summary>
+    /// How the sorter will sort the children
+    /// </summary>
     public Sorting sorting;
 
     /// <summary>
-    /// Sorts all children based on sort type
+    /// Whether to ignore inactive children or not
+    /// </summary>
+    public bool hideInactive = false;
+
+    /// <summary>
+    /// Sorts all children based on sorting type
     /// </summary>
     /// <param name="order">Specify sort order, whether ascending or descending</param>
     public void Sort(SortingOrder order) {
@@ -27,6 +35,7 @@ public class ContentSorter : MonoBehaviour {
         List<Transform> children = new List<Transform>();
         for (int i = 0; i < this.transform.childCount; i++) {
             Transform child = this.transform.GetChild(i);
+            if(hideInactive && !child.gameObject.activeSelf) { continue; }
             children.Add(child);
         }
         
