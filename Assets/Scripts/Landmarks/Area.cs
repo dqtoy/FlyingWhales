db710672c3d270d97928339c7667ec763a3e7553
Mykel Abania <mntabania@gmail.com>
2018-06-20 23:03:55 +0800

    +Initial Areas

---
+++ Assets/Scripts/Landmarks/Area.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area {
+
+    private List<HexTile> _tiles;
+    private HexTile _coreTile;
+
+    public Area(HexTile coreTile) {
+        _coreTile = coreTile;
+        _tiles = new List<HexTile>();
+        AddTile(coreTile);
+    }
+
+    #region Tile Management
+    public void AddTile(HexTile tile) {
+        if (!_tiles.Contains(tile)) {
+            _tiles.Add(tile);
+        }
+    }
+    public void RemoveTile(HexTile tile) {
+        _tiles.Remove(tile);
+    }
+    private void RevalidateTiles() {
+        //TODO: An Area has a primary core tile and any tile that isnt connected to it should also automatically be removed from it.
+    }
+    #endregion
+
+}
