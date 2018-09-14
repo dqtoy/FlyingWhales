aee6e3a007b73889b90a23d9e32c28d5266b0027
Ian Chy <chrisian023@gmail.com>
2018-09-14 16:57:57 +0800

    *intel and secret creator filename, +secret creator

--- Assets/Scripts/Player/Secret.cs
+++ Assets/Scripts/Player/Secret.cs
@@ -9,16 +9,30 @@ public class Secret {
     public int intelIDToBeUnlocked;
 
     private bool _isRevealed;
+    private string _displayText;
 
     #region getters/setters
     public bool isRevealed {
         get { return _isRevealed; }
     }
+    public string displayText {
+        get { return _displayText; }
+    }
     #endregion
 
+    public Secret() {
+        _displayText = "???";
+    }
+    public void SetData(SecretComponent secretComponent) {
+        id = secretComponent.id;
+        name = secretComponent.thisName;
+        description = secretComponent.description;
+        intelIDToBeUnlocked = secretComponent.intelIDToBeUnlocked;
+    }
     public void RevealSecret() {
         if (!_isRevealed) {
             _isRevealed = true;
+            _displayText = name;
             if(intelIDToBeUnlocked != -1) {
                 Intel intel = IntelManager.Instance.intelLookup[intelIDToBeUnlocked];
                 PlayerManager.Instance.player.AddIntel(intel);
