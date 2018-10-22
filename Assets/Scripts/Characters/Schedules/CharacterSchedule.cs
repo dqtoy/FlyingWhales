cf47daa736c7e7e2e7230964ea6b627a908f356d
Mykel Abania <mntabania@gmail.com>
2018-10-22 13:50:18 +0800

    *Remove scheduled movement from characters in test map

--- Assets/Scripts/Characters/Schedules/CharacterSchedule.cs
+++ Assets/Scripts/Characters/Schedules/CharacterSchedule.cs
@@ -147,7 +147,10 @@ public class CharacterSchedule {
         currentPhaseIndex = 0;
         previousPhaseIndex = -1;
         owner.OnSchedulePhaseStarted(currentPhase);
-        Messenger.AddListener(Signals.DAY_START, OnDayStarted);
+        if (owner.homeLandmark != null) {
+            owner.party.actionData.ForceDoAction(ObjectManager.Instance.CreateNewCharacterAction(ACTION_TYPE.REST), owner.homeLandmark.landmarkObj);
+        }
+        //Messenger.AddListener(Signals.DAY_START, OnDayStarted);
     }
 
     private void OnDayStarted() {
@@ -164,84 +167,6 @@ public class CharacterSchedule {
             return currentPhaseIndex + 1;
         }
     }
-    //public CharacterSchedule Clone() {
-    //    CharacterSchedule clone = new CharacterSchedule();
-    //    //for (int i = 0; i < this.phases.Count; i++) {
-    //    //    clone.phases.Add(this.phases[i].Clone()); //create a new clone of each phase, and add it to the phases list of the CharacterSchedule clone
-    //    //}
-    //    return clone;
-    //}
-
-    ///*
-    // This is called when a schedule is assigned to a character.
-    //     */
-    //public void Initialize(Character character) {
-    //    owner = character;
-    //    ValidateSchedule();
-    //    Messenger.AddListener(Signals.HOUR_STARTED, WaitForPhaseStart);
-    //    Messenger.AddListener(Signals.HOUR_ENDED, WaitForPhaseEnd);
-    //    StartNextPhase(phases[0]);
-    //}
-
-    //private void ValidateSchedule() {
-    //    int lastStartTick = 0;
-    //    for (int i = 0; i < phases.Count; i++) {
-    //        CharacterScheduleTemplatePhase currentPhase = phases[i];
-    //        //if (lastStartTick == 0) {
-    //        //    lastStartTick = currentPhase.startTick;
-    //        //} else {
-    //        //    if (lastStartTick > currentPhase.startTick) {
-    //        //        throw new System.Exception("There was an error while trying to validate the daily scedule of " + owner.characterClass.className + " " + owner.name + ". Please check schedule for any inconsistencies.");
-    //        //    }
-    //        //}
-    //    }
-    //}
-    //private void SetCurrentPhase(CharacterScheduleTemplatePhase phase) {
-    //    currentPhase = phase;
-    //}
-    //private void StartNextPhase(CharacterScheduleTemplatePhase nextPhase) {
-    //    SetCurrentPhase(nextPhase);
-    //    owner.OnDailySchedulePhaseStarted(nextPhase);
-    //    //this.nextPhase = GetNextPhase(); //set next phase to the next phase
-    //    //TODO: Change this to use scheduling instead, when the scheduling manager has been converted to handle functions at start of ticks too
-    //    //if (Messenger.eventTable.ContainsKey(Signals.HOUR_STARTED)) {
-    //    //    Messenger.RemoveListener(Signals.HOUR_STARTED, WaitForPhaseStart);
-    //    //}
-    //    //Messenger.AddListener(Signals.HOUR_ENDED, WaitForPhaseEnd);
-    //    //GameDate phaseEnd = GameManager.Instance.Today();
-    //    //phaseEnd.AddHours(currentPhase.phaseLength);
-    //    //SchedulingManager.Instance.AddEntry(phaseEnd, () => EndCurrentPhase());
-    //}
-    //private void WaitForPhaseEnd() {
-    //    //if (GameManager.Instance.Today().hour == Mathf.Clamp(nextPhase.startTick - 1, 1, GameManager.hoursPerDay)) {
-    //    //    EndCurrentPhase();
-    //    //}
-    //}
-    //private void EndCurrentPhase() {
-    //    owner.OnDailySchedulePhaseEnded(currentPhase);
-    //    //TODO: Change this to use scheduling instead, when the scheduling manager has been converted to handle functions at start of ticks too
-    //    //if (Messenger.eventTable.ContainsKey(Signals.HOUR_ENDED)) {
-    //    //    Messenger.RemoveListener(Signals.HOUR_ENDED, WaitForPhaseEnd);
-    //    //}
-    //    //Messenger.AddListener(Signals.HOUR_STARTED, WaitForPhaseStart);
-    //    //GameDate nextPhaseStart = GameManager.Instance.Today();
-    //    //nextPhaseStart.AddHours(1);
-    //    //SchedulingManager.Instance.AddEntry(nextPhaseStart, () => StartNextPhase(GetNextPhase()));
-    //}
-    //private void WaitForPhaseStart() {
-    //    //if (GameManager.Instance.Today().hour == nextPhase.startTick) {
-    //    //    StartNextPhase(nextPhase);
-    //    //}
-    //}
-    //private CharacterScheduleTemplatePhase GetNextPhase() {
-    //    int currentPhaseIndex = phases.IndexOf(currentPhase);
-    //    if (phases.Count-1 == currentPhaseIndex) {
-    //        //current phase is last phase of the day, return the first phase
-    //        return phases[0];
-    //    } else {
-    //        return phases[currentPhaseIndex + 1];
-    //    }
-    //}
 
     public void OnOwnerDied() {
         Messenger.RemoveListener(Signals.DAY_START, OnDayStarted);
