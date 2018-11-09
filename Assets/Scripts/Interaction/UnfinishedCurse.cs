6a31d9e72912c7e0fb7fd89a50600657724f6120
Mykel Abania <mntabania@gmail.com>
2018-11-09 11:17:38 +0800

    *Unfinished Curse Updated +Added Assigned Objects to Interaction State

--- Assets/Scripts/Interaction/UnfinishedCurse.cs
+++ Assets/Scripts/Interaction/UnfinishedCurse.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class UnfinishedCurse : Interaction {
 
-    private const string endResult1Name = "Curse 1 Completed";
-    private const string endResult2Name = "Curse 2 Completed";
-    private const string endResult3Name = "Curse 3 Completed";
-    private const string endResult4Name = "Curse Failed To Complete";
-    private const string endResult5Name = "Obtain Mana";
+    private const string curseCompleted = "Curse Completed";
+    private const string curseFailedToComplete = "Curse Failed To Complete";
+    private const string curseBackfires = "Curse Backfires";
+    private const string obtainMana = "Obtain Mana";
+    private const string doNothing = "Do nothing";
 
-    public UnfinishedCurse(IInteractable interactable) : base(interactable, INTERACTION_TYPE.UNFINISHED_CURSE, 70) {
+    private WeightedDictionary<string> curseWeights;
+
+    public UnfinishedCurse(IInteractable interactable) : base(interactable, INTERACTION_TYPE.UNFINISHED_CURSE, 100) {
         _name = "Unfinished Curse";
     }
 
     #region Overrides
     public override void CreateStates() {
         if (_interactable is BaseLandmark) {
-
-            InteractionState startState = new InteractionState("State 1", this);
+            ConstructCurseWeights();
+            InteractionState startState = new InteractionState("Start", this);
             //string startStateDesc = "Our imp has reported what appears to be an ancient unfinished curse placed within one of the cemetery mausoleums. We may be able to complete the curse but we aren't aware of what it's actual effect would be, if any.";
             //startState.SetDescription(startStateDesc);
             CreateActionOptions(startState);
@@ -27,50 +29,52 @@ public class UnfinishedCurse : Interaction {
             //startState.SetTimeSchedule(startState.actionOptions[2], dueDate); //default is do nothing
 
             //action option states
-            InteractionState endResult1State = new InteractionState(endResult1Name, this); //raid
-            InteractionState endResult2State = new InteractionState(endResult2Name, this); //successfully cancelled raid
-            InteractionState endResult3State = new InteractionState(endResult3Name, this); //failed to cancel raid
-            InteractionState endResult4State = new InteractionState(endResult4Name, this); //critically failed to cancel raid
-            InteractionState endResult5State = new InteractionState(endResult5Name, this); //empowered raid
-
-            endResult1State.SetEndEffect(() => Curse1CompletedEffect(endResult1State));
-            endResult2State.SetEndEffect(() => Curse2CompletedEffect(endResult2State));
-            endResult3State.SetEndEffect(() => Curse3CompletedEffect(endResult3State));
-            endResult4State.SetEndEffect(() => CurseFailedToCompleteEffect(endResult4State));
-            endResult5State.SetEndEffect(() => ObtainManaEffect(endResult5State));
+            InteractionState curseCompletedState = new InteractionState(curseCompleted, this);
+            InteractionState curseFailedToCompleteState = new InteractionState(curseFailedToComplete, this);
+            InteractionState curseBackfiresState = new InteractionState(curseBackfires, this);
+            InteractionState obtainManaState = new InteractionState(obtainMana, this);
+            InteractionState doNothingState = new InteractionState(doNothing, this);
+            
+            curseCompletedState.SetEndEffect(() => CurseCompletedRewardEffect(curseCompletedState));
+            curseFailedToCompleteState.SetEndEffect(() => CurseFailedToCompleteRewardEffect(curseFailedToCompleteState));
+            curseBackfiresState.SetEndEffect(() => CurseBackfiresRewardEffect(curseBackfiresState));
+            obtainManaState.SetEndEffect(() => ObtainManaRewardEffect(obtainManaState));
+            doNothingState.SetEndEffect(() => DoNothingRewardEffect(doNothingState));
+            
 
             _states.Add(startState.name, startState);
-            _states.Add(endResult1State.name, endResult1State);
-            _states.Add(endResult2State.name, endResult2State);
-            _states.Add(endResult3State.name, endResult3State);
-            _states.Add(endResult4State.name, endResult4State);
-            _states.Add(endResult5State.name, endResult5State);
+            _states.Add(curseCompletedState.name, curseCompletedState);
+            _states.Add(curseBackfiresState.name, curseBackfiresState);
+            _states.Add(doNothingState.name, doNothingState);
+            _states.Add(curseFailedToCompleteState.name, curseFailedToCompleteState);
+            _states.Add(obtainManaState.name, obtainManaState);
 
             SetCurrentState(startState);
         }
     }
     public override void CreateActionOptions(InteractionState state) {
-        if (state.name == "State 1") {
+        if (state.name == "Start") {
             ActionOption completeCurse = new ActionOption {
                 interactionState = state,
                 cost = new ActionOptionCost { amount = 5, currency = CURRENCY.MANA },
-                name = "Attempt to complete the curse.",
-                duration = 10,
-                needsMinion = true,
-                neededObjects = new List<System.Type>() { typeof(Minion) },
+                name = "Attempt to complete the ritual.",
+                duration = 0,
+                needsMinion = false,
+                neededObjects = new List<System.Type>() { typeof(CharacterIntel) },
                 effect = () => CompleteCurseEffect(state),
             };
             ActionOption harnessMagic = new ActionOption {
                 interactionState = state,
                 cost = new ActionOptionCost { amount = 20, currency = CURRENCY.SUPPLY },
                 name = "Harness its magic into Mana.",
-                duration = 10,
-                needsMinion = true,
-                neededObjects = new List<System.Type>() { typeof(Minion) },
+                duration = 0,
+                needsMinion = false,
                 effect = () => HarnessMagicEffect(state),
+                canBeDoneAction = () => AssignedMinionIsOfType(DEMON_TYPE.GLUTTONY)
             };
             ActionOption leaveAlone = new ActionOption {
                 interactionState = state,
+                cost = new ActionOptionCost { amount = 0, currency = CURRENCY.SUPPLY },
                 name = "Leave it alone.",
                 duration = 0,
                 needsMinion = false,
@@ -84,74 +88,72 @@ public class UnfinishedCurse : Interaction {
     }
     #endregion
 
+    #region Action Option Effects
     private void CompleteCurseEffect(InteractionState state) {
         WeightedDictionary<string> effectWeights = new WeightedDictionary<string>();
-        effectWeights.AddElement(endResult1Name, 30);
-        effectWeights.AddElement(endResult2Name, 20);
-        effectWeights.AddElement(endResult3Name, 10);
-        effectWeights.AddElement(endResult4Name, 10);
+        effectWeights.AddElement(curseCompleted, 30);
+        effectWeights.AddElement(curseFailedToComplete, 10);
+        effectWeights.AddElement(curseBackfires, 5);
+
 
         string chosenEffect = effectWeights.PickRandomElementGivenWeights();
-        if (chosenEffect == endResult1Name) {
-            Curse1Completed(state, chosenEffect);
-        } else if (chosenEffect == endResult2Name) {
-            Curse2Completed(state, chosenEffect);
-        } else if (chosenEffect == endResult3Name) {
-            Curse3Completed(state, chosenEffect);
-        } else if (chosenEffect == endResult4Name) {
-            CurseFailedToComplete(state, chosenEffect);
-        }
+        SetCurrentState(_states[chosenEffect]);
+        //if (chosenEffect == curse1Completed) {
+        //    Curse1Completed(state, chosenEffect);
+        //} else if (chosenEffect == curse2Completed) {
+        //    Curse2Completed(state, chosenEffect);
+        //} else if (chosenEffect == curse3Completed) {
+        //    Curse3Completed(state, chosenEffect);
+        //} else if (chosenEffect == curseFailedToComplete) {
+        //    CurseFailedToComplete(state, chosenEffect);
+        //}
     }
     private void HarnessMagicEffect(InteractionState state) {
         WeightedDictionary<string> effectWeights = new WeightedDictionary<string>();
-        effectWeights.AddElement(endResult5Name, 10);
+        effectWeights.AddElement(obtainMana, 10);
 
         string chosenEffect = effectWeights.PickRandomElementGivenWeights();
-        if (chosenEffect == endResult5Name) {
-            Curse1Completed(state, chosenEffect);
-        }
+        SetCurrentState(_states[chosenEffect]);
+        //if (chosenEffect == obtainMana) {
+        //    CurseCompleted(state, chosenEffect);
+        //}
     }
     private void LeaveItAloneEffect(InteractionState state) {
-        state.EndResult(); //Immediately ends interaction
+        SetCurrentState(_states[doNothing]);
     }
+    #endregion
 
-    private void Curse1Completed(InteractionState state, string effectName) {
-        //_states[effectName].SetDescription(state.chosenOption.assignedMinion.icharacter.name + " tried a couple of chants to complete the curse. After the latest one, a violent thunderstrike sounded from the distance.");
-        SetCurrentState(_states[effectName]);
-    }
-    private void Curse1CompletedEffect(InteractionState state) {
+    private void CurseCompletedRewardEffect(InteractionState state) {
         state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1)); //**Reward**: Demon gains Exp 1
+        //**Effect**: Character should gain a random curse from the Curse checklist below
+        string chosenCurse = curseWeights.PickRandomElementGivenWeights();
+        state.assignedCharacter.character.AddCombatAttribute(AttributeManager.Instance.allCombatAttributes[chosenCurse]);
     }
-    private void Curse2Completed(InteractionState state, string effectName) {
-        //_states[effectName].SetDescription(state.chosenOption.assignedMinion.icharacter.name + " tried a couple of chants to complete the curse. After the latest one, a violent thunderstrike sounded from the distance.");
-        SetCurrentState(_states[effectName]);
+    private void CurseFailedToCompleteRewardEffect(InteractionState state) {
+        //**Reward**: Demon gains Exp 1
+        state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1));
     }
-    private void Curse2CompletedEffect(InteractionState state) {
+    private void CurseBackfiresRewardEffect(InteractionState state) {
         state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1)); //**Reward**: Demon gains Exp 1
+        //**Effect**: Demon Minion should gain a random curse from the Curse checklist below
+        string chosenCurse = curseWeights.PickRandomElementGivenWeights();
+        state.assignedMinion.icharacter.AddCombatAttribute(AttributeManager.Instance.allCombatAttributes[chosenCurse]);
     }
-    private void Curse3Completed(InteractionState state, string effectName) {
-        //_states[effectName].SetDescription(state.chosenOption.assignedMinion.icharacter.name + " tried a couple of chants to complete the curse. After the latest one, a violent thunderstrike sounded from the distance.");
-        SetCurrentState(_states[effectName]);
-    }
-    private void Curse3CompletedEffect(InteractionState state) {
-        state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1)); //**Reward**: Demon gains Exp 1
-    }
-    private void CurseFailedToComplete(InteractionState state, string effectName) {
-        //_states[effectName].SetDescription(state.chosenOption.assignedMinion.icharacter.name + " attempted to complete the ancient curse but failed to figure out the proper chant to make it work.");
-        SetCurrentState(_states[effectName]);
-    }
-    private void CurseFailedToCompleteEffect(InteractionState state) {
-        state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1)); //**Reward**: Demon gains Exp 1
-    }
-
-    private void ObtainMana(InteractionState state, string effectName) {
-        //_states[effectName].SetDescription(state.chosenOption.assignedMinion.icharacter.name + " discovered a source of magical energy. We have converted it into a small amount of Mana.");
-        SetCurrentState(_states[effectName]);
-    }
-    private void ObtainManaEffect(InteractionState state) {
+    private void ObtainManaRewardEffect(InteractionState state) {
         //**Reward**: Mana Cache 1, Demon gains Exp 1
         state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1));
         Reward reward = InteractionManager.Instance.GetReward(InteractionManager.Mana_Cache_Reward_1);
         PlayerManager.Instance.player.ClaimReward(reward);
+    }
+    private void DoNothingRewardEffect(InteractionState state) {
+        //**Reward**: Demon gains Exp 1
+        state.assignedMinion.ClaimReward(InteractionManager.Instance.GetReward(InteractionManager.Exp_Reward_1));
+    }
+
+    private void ConstructCurseWeights() {
+        curseWeights = new WeightedDictionary<string>();
+        curseWeights.AddElement("Placeholder Curse 1", 20);
+        curseWeights.AddElement("Placeholder Curse 2", 30);
+        curseWeights.AddElement("Placeholder Curse 3", 5);
     }
 }
