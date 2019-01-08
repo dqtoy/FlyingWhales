03fb0209199997872929264508ddfe9b780f9c40
Mykel Abania <mntabania@gmail.com>
2019-01-08 17:10:13 +0800

    +New Main Menu

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour {
+
+    [SerializeField] private EasyTween buttonsTween;
+    [SerializeField] private EasyTween titleTween;
+
+    public void Start() {
+        buttonsTween.OnValueChangedAnimation(true);
+        titleTween.OnValueChangedAnimation(true);
+    }
+}
---
+++ Assets/Scripts/UI/PingPongCanvasGroupAlpha.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongCanvasGroupAlpha : MonoBehaviour {
+
+    public CanvasGroup target;
+    public float speed = 1f;
+
+    private float _t = 0f;
+
+    void Update() {
+        _t += Time.deltaTime * speed;
+        target.alpha = Mathf.PingPong(_t, 1f);
+    }
+
+}
