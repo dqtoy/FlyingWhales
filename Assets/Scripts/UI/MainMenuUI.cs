a41618f81bd1367e722dc16e3fcb2087d9734cb1
Mykel Abania <mntabania@gmail.com>
2019-01-21 07:36:24 +0800

    +Updated Main Menu (Problem with credits on hover button)

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -8,13 +8,35 @@ public class MainMenuUI : MonoBehaviour {
     [SerializeField] private EasyTween buttonsTween;
     [SerializeField] private EasyTween titleTween;
 
+    [SerializeField] private EasyTween glowTween;
+    [SerializeField] private EasyTween glow2Tween;
+
     public void Start() {
         buttonsTween.OnValueChangedAnimation(true);
         titleTween.OnValueChangedAnimation(true);
+        glowTween.OnValueChangedAnimation(true);
     }
 
     public void ExitGame() {
         //TODO: Add Confirmation Prompt
         Application.Quit();
     }
+
+    public void Glow2TweenPlayForward() {
+        glow2Tween.OnValueChangedAnimation(true);
+    }
+
+    public void Glow2TweenPlayReverse() {
+        glow2Tween.OnValueChangedAnimation(false);
+    }
+
+    public void GlowTweenPlayForward() {
+        glowTween.OnValueChangedAnimation(true);
+    }
+
+    public void GlowTweenPlayReverse() {
+        glowTween.OnValueChangedAnimation(false);
+    }
+
+
 }
