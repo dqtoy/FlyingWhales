e7e1720f41f6f2a39101ea740179f025e47d89d0
Ian Chy <chrisian023@gmail.com>
2019-04-04 19:42:17 +0800

    *character state comments for readability

--- Assets/Scripts/Characters/CharacterState/CharacterStateComponent.cs
+++ Assets/Scripts/Characters/CharacterState/CharacterStateComponent.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This is the bridge between the character and the states, a component that stores all necessary information and process all data for the character and its states  
+//Basically, this is a manager for the character states
+//This is where you switch from one state to another, etc.
+//Everything a character wants to do with a state must go through here
 public class CharacterStateComponent {
-    public string previousMajorStateName;
-    public string previousStateName;
-    public string currentStateName;
+    //public string previousMajorStateName;
+    //public string previousStateName;
+    //public string currentStateName;
 
     public Character character { get; private set; }
+    //If a major state is replaced by a minor state, must be stored in order for the character to go back to this state after doing the minor state
     public CharacterState previousMajorState { get; private set; }
+    //This is the character's current state
     public CharacterState currentState { get; private set; }
 
     public CharacterStateComponent(Character character) {
         this.character = character;
     }
+
+    //This switches from one state to another
+    //If the character is not in a state right now, this simply starts a new state instead of switching
     public void SwitchToState(CHARACTER_STATE state, Character targetCharacter = null) {
+        //Before switching character must end current action first because once a character is in a state in cannot make plans
         character.AdjustIsWaitingForInteraction(1);
         if (character.currentAction != null && !character.currentAction.isDone) {
             if (!character.currentAction.isPerformingActualAction) {
@@ -25,6 +35,7 @@ public class CharacterStateComponent {
         }
         character.AdjustIsWaitingForInteraction(-1);
 
+        //Stop the movement of character because the new state probably has different movement behavior
         if(character.currentParty.icon.isTravelling) {
             if (character.currentParty.icon.travelLine == null) {
                 character.marker.StopMovementOnly();
@@ -32,32 +43,39 @@ public class CharacterStateComponent {
                 //TODO: What if the character travelling to an area
             }
         }
-        CharacterState newState = CreateNewState(state);
 
+        //Create the new state
+        CharacterState newState = CreateNewState(state);
         if (currentState != null) {
-            previousStateName = currentState.stateName;
             if (currentState.stateCategory == CHARACTER_STATE_CATEGORY.MAJOR) {
-                previousMajorStateName = currentState.stateName;
                 if(newState.stateCategory == CHARACTER_STATE_CATEGORY.MAJOR) {
                     //If the new state is a major state, automatically end current major state, do not just pause it
+                    //Also, do not store the previous major state because since the new state is a major state, it must not go back to the previous major state
                     previousMajorState = null;
                     currentState.ExitState();
                 } else {
+                    //Store current state as previous major state so the character will go back to that state after doing the minor state
+                    //Pause the previous major state so that the timer will not tick, if there's any
                     previousMajorState = currentState;
                     previousMajorState.PauseState();
                 }
             } else {
+                //If current state is a minor state, simply end it
                 currentState.ExitState();
             }
 
         }
 
+        //Assigns new state as the current state then enter that state
         currentState = newState;
         currentState.SetTargetCharacter(targetCharacter);
-        currentStateName = currentState.stateName;
         currentState.EnterState();
     }
+
+    //This ends the current state
+    //This is triggered when the timer is out, or the character simply wants to end its state and go back to normal state
     public void ExitCurrentState(CharacterState state, bool stopMovement = true) {
+        //Stops movement unless told otherwise
         if (stopMovement) {
             if (character.currentParty.icon.isTravelling) {
                 if (character.currentParty.icon.travelLine == null) {
@@ -68,23 +86,29 @@ public class CharacterStateComponent {
             }
         }
         if (currentState != null) {
+            //This ends the current state but I added a checker that the parameter state must be the same as the current state to avoid inconsistencies
             if(currentState != state) {
                 Debug.LogError("Inconsistency! The current state " + currentState.stateName + " of " + character.name + " does not match the state " + state.stateName);
                 return;
             }
-            previousStateName = currentState.stateName;
-            if (currentState.stateCategory == CHARACTER_STATE_CATEGORY.MAJOR) {
-                //previousMajorState = currentState;
-                previousMajorStateName = currentState.stateName; 
-            }
             currentState.ExitState();
-
         }
+
+        //If the current state is a minor state and there is a previous major state, resume that major state
         if(currentState.stateCategory == CHARACTER_STATE_CATEGORY.MINOR && previousMajorState != null) {
-            currentState = previousMajorState;
-            currentStateName = currentState.stateName;
-            currentState.ResumeState();
+            if(previousMajorState.duration > 0 && previousMajorState.currentDuration >= previousMajorState.duration) {
+                //In a rare case that the previous major state has already timed out, end that state and do not resume it
+                //This goes back to normal
+                previousMajorState.ExitState();
+                currentState = null;
+            } else {
+                //Resumes previous major state
+                currentState = previousMajorState;
+                currentState.ResumeState();
+            }
+            previousMajorState = null;
         } else {
+            //This goes back to normal
             currentState = null;
         }
     }
