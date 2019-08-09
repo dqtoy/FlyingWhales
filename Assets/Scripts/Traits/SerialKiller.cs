9c3b9e2d373663005ef8cc37caf79bf2504a39c7
Ian Chy <chrisian023@gmail.com>
2019-08-09 19:46:06 +0800

    *serial killer next victim setter

--- Assets/Scripts/Traits/SerialKiller.cs
+++ Assets/Scripts/Traits/SerialKiller.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SerialKiller : Trait {
 
-    public SerialVictim victim1 { get; private set; }
-    public SerialVictim victim2 { get; private set; }
+    public SerialVictim victim1Requirement { get; private set; }
+    public SerialVictim victim2Requirement { get; private set; }
     public Character character { get; private set; }
+
+    public Character targetVictim { get; private set; }
 
     public SerialKiller() {
         name = "Serial Killer";
@@ -37,16 +39,37 @@ public class SerialKiller : Trait {
     public override bool CreateJobsOnEnterVisionBasedOnOwnerTrait(IPointOfInterest targetPOI, Character characterThatWillDoJob) {
         return base.CreateJobsOnEnterVisionBasedOnOwnerTrait(targetPOI, characterThatWillDoJob);
     }
+    public override void OnSeePOI(IPointOfInterest targetPOI, Character character) {
+        base.OnSeePOI(targetPOI, character);
+        if(targetPOI is Character) {
+            Character potentialVictim = targetPOI as Character;
+            if (targetVictim != null) {
+                if (targetVictim.specificLocation != this.character.specificLocation || targetVictim.isDead) {
+                    targetVictim = null;
+                }
+            }
+            if (targetVictim == null) {
+                if (DoesCharacterFitAnyVictimRequirements(potentialVictim)) {
+                    targetVictim = potentialVictim;
+
+                    Log log = new Log(GameManager.Instance.Today(), "Character", "NonIntel", "serial_killer_new_victim");
+                    log.AddToFillers(this.character, this.character.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
+                    log.AddToFillers(targetVictim, targetVictim.name, LOG_IDENTIFIER.TARGET_CHARACTER);
+                    this.character.RegisterLogAndShowNotifToThisCharacterOnly(log, onlyClickedCharacter: false);
+                }
+            }
+        }
+    }
     #endregion
 
     private void GenerateSerialVictims() {
-        victim1 = new SerialVictim(RandomizeVictimType(true), RandomizeVictimType(false));
-        victim2 = new SerialVictim(RandomizeVictimType(true), RandomizeVictimType(false));
+        victim1Requirement = new SerialVictim(RandomizeVictimType(true), RandomizeVictimType(false));
+        victim2Requirement = new SerialVictim(RandomizeVictimType(true), RandomizeVictimType(false));
 
         Log log = new Log(GameManager.Instance.Today(), "Character", "NonIntel", "became_serial_killer");
         log.AddToFillers(character, character.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
-        log.AddToFillers(null, victim1.text, LOG_IDENTIFIER.STRING_1);
-        log.AddToFillers(null, victim2.text, LOG_IDENTIFIER.STRING_2);
+        log.AddToFillers(null, victim1Requirement.text, LOG_IDENTIFIER.STRING_1);
+        log.AddToFillers(null, victim2Requirement.text, LOG_IDENTIFIER.STRING_2);
         log.AddLogToInvolvedObjects();
         PlayerManager.Instance.player.ShowNotification(log);
     }
@@ -64,6 +87,11 @@ public class SerialKiller : Trait {
             }
             return SERIAL_VICTIM_TYPE.STATUS;
         }
+    }
+
+    private bool DoesCharacterFitAnyVictimRequirements(Character target) {
+        return victim1Requirement.DoesCharacterFitVictimRequirements(target)
+            || victim2Requirement.DoesCharacterFitVictimRequirements(target);
     }
 }
 
@@ -93,10 +121,10 @@ public class SerialVictim {
         } else if (victimType == SERIAL_VICTIM_TYPE.ROLE) {
             CHARACTER_ROLE[] roles = new CHARACTER_ROLE[] { CHARACTER_ROLE.CIVILIAN, CHARACTER_ROLE.SOLDIER, CHARACTER_ROLE.ADVENTURER };
             return roles[UnityEngine.Random.Range(0, roles.Length)].ToString();
-        } else if (victimType == SERIAL_VICTIM_TYPE.STATUS) {
+        } else if (victimType == SERIAL_VICTIM_TYPE.TRAIT) {
             string[] traits = new string[] { "Craftsman", "Criminal", "Drunk", "Sick", "Lazy", "Hardworking", "Curious" };
             return traits[UnityEngine.Random.Range(0, traits.Length)];
-        } else if (victimType == SERIAL_VICTIM_TYPE.TRAIT) {
+        } else if (victimType == SERIAL_VICTIM_TYPE.STATUS) {
             string[] statuses = new string[] { "Hungry", "Tired", "Lonely" };
             return statuses[UnityEngine.Random.Range(0, statuses.Length)];
         }
@@ -121,5 +149,22 @@ public class SerialVictim {
             newText = text.Replace("man", "men");
         }
         return newText;
+    }
+
+    public bool DoesCharacterFitVictimRequirements(Character character) {
+        return DoesCharacterFitVictimTypeDescription(victimFirstType, victimFirstDescription, character)
+            && DoesCharacterFitVictimTypeDescription(victimSecondType, victimSecondDescription, character);
+    }
+    private bool DoesCharacterFitVictimTypeDescription(SERIAL_VICTIM_TYPE victimType, string victimDesc, Character character) {
+        if (victimType == SERIAL_VICTIM_TYPE.GENDER) {
+            return victimDesc == character.gender.ToString();
+        } else if (victimType == SERIAL_VICTIM_TYPE.ROLE) {
+            return victimDesc == character.role.roleType.ToString();
+        } else if (victimType == SERIAL_VICTIM_TYPE.TRAIT) {
+            return character.GetNormalTrait(victimDesc) != null;
+        } else if (victimType == SERIAL_VICTIM_TYPE.STATUS) {
+            return character.GetNormalTrait(victimDesc) != null;
+        }
+        return false;
     }
 }
