241a3064fe307fcae92c5d9b54855941c9c2734a
Mykel Abania <mntabania@gmail.com>
2020-01-04 16:22:52 +0800

    **Fixes**
     - Agoraphobic Trigger Flaw
     - Coward Trigger Flaw
     - Skeleton assets

--- Assets/Scripts/Traits/Normal Traits/Agoraphobic.cs
+++ Assets/Scripts/Traits/Normal Traits/Agoraphobic.cs
@@ -65,18 +65,19 @@ namespace Traits {
             string successLogKey = base.TriggerFlaw(character);
             if (character.homeStructure != null) {
                 if (character.currentStructure != character.homeStructure) {
-                    if (character.currentActionNode.action != null) {
+                    if (character.currentActionNode != null) {
                         character.StopCurrentActionNode(false);
                     }
                     if (character.stateComponent.currentState != null) {
                         character.stateComponent.ExitCurrentState();
-                    } 
-                    //else if (character.stateComponent.stateToDo != null) {
-                    //    character.stateComponent.SetStateToDo(null, false, false);
-                    //}
-
-                    LocationGridTile tile = character.homeStructure.tiles[Random.Range(0, character.homeStructure.tiles.Count)];
-                    character.marker.GoTo(tile);
+                    }
+                    ActualGoapNode node = new ActualGoapNode(InteractionManager.Instance.goapActionData[INTERACTION_TYPE.RETURN_HOME], character, character, null, 0);
+                    GoapPlan goapPlan = new GoapPlan(new List<JobNode>() { new SingleJobNode(node) }, character);
+                    GoapPlanJob job = JobManager.Instance.CreateNewGoapPlanJob(JOB_TYPE.TRIGGER_FLAW, INTERACTION_TYPE.RETURN_HOME, character, character);
+                    goapPlan.SetDoNotRecalculate(true);
+                    job.SetCannotBePushedBack(true);
+                    job.SetAssignedPlan(goapPlan);
+                    character.jobQueue.AddJobInQueue(job);
                     return successLogKey;
                 } else {
                     return "fail_at_home";
