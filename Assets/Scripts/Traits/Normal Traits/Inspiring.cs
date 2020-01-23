cdca6c3fcf5bb55eb189a22ce7702d88107bdb44
Ian Chy <chrisian023@gmail.com>
2020-01-23 11:34:54 +0800

    *debug logs

--- Assets/Scripts/Traits/Normal Traits/Inspiring.cs
+++ Assets/Scripts/Traits/Normal Traits/Inspiring.cs
@@ -16,12 +16,22 @@ namespace Traits {
         public override bool OnSeePOI(IPointOfInterest targetPOI, Character characterThatWillDoJob) {
             if (targetPOI is Character) {
                 Character targetCharacter = targetPOI as Character;
+                string debugLog = characterThatWillDoJob.name + " saw " + targetPOI.name + " and has " + name;
+                debugLog += "\n-20% chance to trigger Inspired interrupt if Target is part of Faction or Settlement";
                 if (characterThatWillDoJob.faction == targetCharacter.faction || characterThatWillDoJob.homeSettlement == targetCharacter.homeSettlement) {
-                    if (UnityEngine.Random.Range(0, 100) < 8) {
+                    debugLog += "\n-Target is part of Faction or Settlement";
+                    int chance = UnityEngine.Random.Range(0, 100);
+                    debugLog += "\n-Roll: " + chance;
+                    if (chance < 8) {
+                        debugLog += "\n-Triggered Inspired interrupt";
+                        characterThatWillDoJob.PrintLogIfActive(debugLog);
                         targetCharacter.interruptComponent.TriggerInterrupt(INTERRUPT.Inspired, characterThatWillDoJob);
                         return true;
                     }
+                } else {
+                    debugLog += "\n-Target is part of Faction or Settlement";
                 }
+                characterThatWillDoJob.PrintLogIfActive(debugLog);
             }
             return base.OnSeePOI(targetPOI, characterThatWillDoJob);
         }
