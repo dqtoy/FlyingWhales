210cc7701efcc2dba38875940ddd5caf1fde67c9
Ian Chy <chrisian023@gmail.com>
2020-02-11 18:23:02 +0800

    *item bug fixes

---
+++ Assets/Scripts/Locations/Inner Maps/Map Objects/Tile Objects/Tool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : TileObject {
+
+    public Tool() {
+        advertisedActions = new List<INTERACTION_TYPE>();
+        Initialize(TILE_OBJECT_TYPE.TOOL);
+        RemoveCommonAdvertisements();
+    }
+    public Tool(SaveDataTileObject data) {
+        advertisedActions = new List<INTERACTION_TYPE>();
+        Initialize(data);
+    }
+}
